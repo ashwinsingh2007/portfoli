d13e68a1fb5d128bcf6c38bdfc09f198ff1b30a5
Ashwin Singh <legend.ashwini07@gmail.com>
2019-06-07 11:14:13 +0530

    Updates

---
+++ precache-manifest.61703eab2ed5a0b0ddd38bff2cfee598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde4c1bbffda92eb26d6",
+    "url": "/portfoli/static/js/runtime~main.bde4c1bb.js"
+  },
+  {
+    "revision": "33b9bdaf9ad2f5f98d34",
+    "url": "/portfoli/static/js/main.33b9bdaf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfoli/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "33b9bdaf9ad2f5f98d34",
+    "url": "/portfoli/static/css/main.8a9e6962.chunk.css"
+  },
+  {
+    "revision": "e9bb4bf30da0d275f23d62dc57b35128",
+    "url": "/portfoli/index.html"
+  }
+];
