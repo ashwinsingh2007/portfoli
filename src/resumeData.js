f3b9196fcd4bf9e591fb7d35fd1a9f68e4fb0d28
Ashwin Singh <legend.ashwini07@gmail.com>
2019-06-07 11:09:51 +0530

    my portfoli

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Ashwani Kumar",
+  role: "",
+  linkedinId: "https://www.linkedin.com/in/ashwani-kumar-97465186/",
+  skypeid: "Your skypeid",
+  roleDescription: "Full Stack Dev | Open-Source Enthusiast | Blogger | Ninja",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ashwani-kumar-97465186/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/ashwinsingh2007",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "https://twitter.com/ashwinigithub",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme: `I like JavaScript and everything web. When my dev senses kick-in I build presumably awesome stuff. I stay close to the community and try to keep tabs with the pace at which the web is evolving. I also like to blog what I learn.
+    React-Redux, Node.js, Docker, Google cloud are the main tricks up my sleeve. I am also obsessed with making the web look pretty with SASS/CSS.
+    I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. 
+    I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.`,
+  address: "India",
+  website: "https://github.com/ashwinsingh2007",
+  education: [
+    {
+      UniversityName: "LNCT Bhopal",
+      specialization: "Computer Science",
+      MonthOfPassing: "June",
+      YearOfPassing: "2016",
+      Achievements: ""
+    }
+  ],
+  work: [
+    {
+      CompanyName: "PeopleGrove",
+      specialization: "Full stack developer",
+      MonthOfLeaving: "Currently Working",
+      YearOfLeaving: "",
+      Achievements: ""
+    },
+    {
+      CompanyName: "Persistent Systems",
+      specialization: "Software Engineer",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2017",
+      Achievements: ""
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "ReactJs"
+    },
+    {
+      skillname: "React-Redux-Saga"
+    },
+    {
+      skillname: "NodeJS"
+    },
+    {
+      skillname: "Python"
+    },
+    {
+      skillname: "Machine-Learning"
+    }
+  ],
+  portfolio: [
+    {
+      name: "React-MusicPlayer",
+      description: "Music Playe made in react",
+      imgurl: "https://img.rasset.ie/000fd53c-500.jpg",
+      url: "https://ashwinsingh2007.github.io/React-MusicPlayer-12-hr-challenge-/index.html"
+    },
+    {
+      name: "React-Redux-Saga-weather-app",
+      description: "Create-react-app weather app using redux, redux-sagas, and darksky weather api.",
+      imgurl: "https://wptravelengine.com/wp-content/uploads/edd/2018/12/trip-weather-forecast-banner-1000x750.jpg",
+      url: "https://ashwinsingh2007.github.io/react-redux-saga-weather-app/index.html"
+
+    },
+    {
+      name: "Node Wallpaper Scrapper",
+      description: "A NodeJs cli app for searching for (preferably) lax licensed wallpapers.Scrappy can currently scrape unsplash.com, wallhaven.cc, photocollections.io & magdeleine.co",
+      imgurl: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/101433995/original/b4953662b732f019b932e5b4562bda68fa4155c9/create-a-nodejs-web-scraper-for-a-site-that-suits-your-needs.png",
+      url: "https://github.com/ashwinsingh2007/wallpaper-scrapper"
+    },
+    {
+      name: "Input trigger React",
+      description: "Useful for building applications that need Slack-like emoji suggestions (triggered by typing :) or Github-like user mentions (triggered by typing @).",
+      imgurl: "https://www.sketchappsources.com/resources/source-image/facebook-reaction-buttons-artsvit.jpg",
+      url: "https://ashwinsingh2007.github.io/InputTrigger/index.html"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
